fd861d35eaf62ce5c8a23a18183388dbde3bf820
Kostya70 <Mirror50s@mail.ru>
2023-03-09 16:49:55 +0500

    add concurrency lesson

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,15 +1,31 @@
 public class Counter {
     private int  value;
+    private int  value2;
 
-    public void inc(){
+    private Object monitor1 = new Object();
+    private Object monitor2 = new Object();
+
+    public synchronized void inc(){
         value++;
     }
 
-    public void dec(){
+    public synchronized void dec(){
         value--;
     }
 
     public int getValue() {
+        return value;
+    }
+
+    public synchronized void inc2(){
+        value++;
+    }
+
+    public synchronized void dec2(){
+        value--;
+    }
+
+    public int getValue2() {
         return value;
     }
 }
--- src/main/java/MainCounter.java
+++ src/main/java/MainCounter.java
@@ -1,10 +1,12 @@
 public class MainCounter {
     public static void main(String[] args) {
         Counter counter = new Counter();
+        long before = System.currentTimeMillis();
+        int barrier = 10_000_000;
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 1000; i++) {
+                for (int i = 0; i < barrier; i++) {
                     counter.inc();
                 }
             }
@@ -12,19 +14,42 @@ public class MainCounter {
         Thread thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 1000; i++) {
+                for (int i = 0; i < barrier; i++) {
                     counter.dec();
+                }
+            }
+        });
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < barrier; i++) {
+                    counter.inc2();
+                }
+            }
+        });
+        Thread thread4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < barrier; i++) {
+                    counter.dec2();
                 }
             }
         });
         thread1.start();
         thread2.start();
+        thread3.start();
+        thread4.start();
         try {
             thread1.join();
             thread2.join();
+            thread3.join();
+            thread4.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         System.out.println(counter.getValue());
+        System.out.println(counter.getValue2());
+        long after = System.currentTimeMillis();
+        System.out.println(after - before);
     }
 }
